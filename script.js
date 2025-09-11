d690f6a16ac34d50a37eb99d58725706aeb55461
jaoziwn-a11y <jaoziwn@gmail.com>
2025-09-11 20:39:24 -0300

    refactor: simplify script by removing unused variables and adding type comparison

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
-/* let nome = "João";
-let idade = 17;
-let cidade = "Campinas - SP";
-let altura = 1.73;
-let ehhumano = true;
-
-console.log("Nome: " + nome);
-console.log("Idade: " + idade);
-console.log("Cidade: " + cidade);
-console.log("Altura: " + altura);
-console.log("É humano? " + ehhumano);
-
-console.log(typeof nome);
-console.log(typeof idade);
-console.log(typeof cidade);
-console.log(typeof altura);
-console.log(typeof ehhumano);
-
-console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade}, minha altura é ${altura}m e é humano: ${ehhumano}`);
-
-
-let texto = "Olá, mundo!";
-let numero = 42;
-let pi = 3.14;
-let booleano = true;
-let nulo = null;
-let indefinido;
-
-console.log(typeof texto);        // string
-console.log(typeof numero);      // number      
-console.log(typeof pi);          // number
-console.log(typeof booleano);    // boolean
-console.log(typeof nulo);        // object (isso é um comportamento conhecido do JavaScript)
-console.log(typeof indefinido);  // undefined
-*/
-
-let nome = "João";
-let idade = 17;
-let altura = 1.73;
-let estudante = true;
-let endereco = "Frederico Weege, 1029, Pomerode Fundos - SC"
-
-console.log(`Nome: ${nome}`);
-console.log(`Idade: ${idade}`);
-console.log(`Altura: ${altura}m`);
-console.log(`É estudante? ${estudante}`);
-console.log(`Endereço: ${endereco}`);s
+let a = 5
+let b ="5"
+if (a === b){
+console.log ("true");
+}else {
+    console.log ("false");
+}
